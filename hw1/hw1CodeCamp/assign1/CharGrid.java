2cbdd12389ee29e546d7aa015c3126c99997ce49
leontian <leontian@users.noreply.github.com>
2015-06-12 11:12:56 -0400

    hw1 chargrid

--- hw1/hw1CodeCamp/assign1/CharGrid.java
+++ hw1/hw1CodeCamp/assign1/CharGrid.java
@@ -22,11 +22,45 @@ public class CharGrid {
 	 * @return area for given char
 	 */
 	public int charArea(char ch) {
-	    int minx, miny, maxx, maxy;
-	    for(int i = 0; i < this.grid.length; i++) {
-	        
+	    int minx = this.grid.length;
+	    int miny;
+	    if(minx > 0) {
+	        miny = grid[0].length;
 	    }
-		return 0;
+	    else {
+	        miny = 0;
+	    }
+ 
+	    int maxx = 0;
+	    int maxy = 0;
+	    
+	    for(int i = 0; i < this.grid.length; i++) {
+	        if(this.grid.length == 0) {
+	            return 0;
+	        }
+	        for(int j = 0; j < this.grid[0].length; j++) {
+	            if(grid[i][j] == ch) {
+	                if(i < minx) {
+	                    minx = i;
+	                }
+	                if(i > maxx) {
+	                    maxx = i;
+	                }
+	                if(j < miny) {
+	                    miny = j;
+	                }
+	                if (j > maxy) {
+	                    maxy = j;
+	                }
+	            }
+	        }	        
+	    }
+	    if(maxx >= minx && maxy >= miny) {
+	        return (maxx - minx + 1) * (maxy - miny + 1);
+	    }
+	    else {
+	        return 0;
+	    }
 	}
 	
 	/**
@@ -34,7 +68,38 @@ public class CharGrid {
 	 * @return number of + in grid
 	 */
 	public int countPlus() {
-		return 0; // TODO ADD YOUR CODE HERE
+	    if(this.grid.length < 3 || this.grid[0].length < 3) {
+	        return 0;
+	    }
+	    int count = 0;
+	    int row = this.grid.length;
+	    int col = this.grid[0].length;
+	    for(int i = 1; i < row; i++) {
+	        for(int j = 0; j < col; j++) {
+	            int k = armLength(i, j, -1, 0);
+	            if(k > 1 & k == armLength(i, j, 0, 1) 
+	                    && k == armLength(i, j, 1, 0) 
+	                    && k == armLength(i, j, 0, -1)) {
+	                count++;
+	            }
+	        }
+	    }
+	    return count;
+	}
+
+	private int armLength(int i, int j, int x, int y) {
+	    int k = 1;
+	    char ch = this.grid[i][j];
+	    while(i+k*x >= 0 && i+k*x <= this.grid.length-1
+	            && j+k*y >= 0 && j+k*y <= this.grid[0].length-1) {
+	        if(this.grid[i+k*x][j+k*y] == ch) {
+	            k++;
+	        }
+	        else {
+	            break;
+	        }
+	    }
+	    return k;
 	}
 	
 }
--- hw1/hw1CodeCamp/assign1/CharGridTest.java
+++ hw1/hw1CodeCamp/assign1/CharGridTest.java
@@ -34,8 +34,70 @@ public class CharGridTest {
 		assertEquals(6, cg.charArea('a'));
 		assertEquals(3, cg.charArea('b'));
 		assertEquals(1, cg.charArea('c'));
+		assertEquals(0, cg.charArea('d'));
 	}
 	
-	// TODO Add more tests
-	
+    @Test
+    //edge, zero
+    public void testCountPlus1() {
+        char[][] grid = new char[][] {
+                {'c', 'a', ' '},
+                {'b', ' ', 'b'},
+                {' ', ' ', 'a'}
+            };
+        char[][] grid2 = new char[][] {
+                {'c', 'b', ' '},
+                {'b', 'b', 'b'},
+                {' ', 'b', 'a'},
+                {' ', 'b', 'a'}
+            };
+        
+        CharGrid cg = new CharGrid(grid);
+        CharGrid cg2 = new CharGrid(grid2);
+        
+        assertEquals(0, cg.countPlus());
+        assertEquals(0, cg2.countPlus());
+
+    }
+    @Test
+    //single plus
+    public void testCountPlus2() {
+        char[][] grid = new char[][] {
+                {'c', 'b', ' '},
+                {'b', 'b', 'b'},
+                {' ', 'b', 'a'}
+            };
+        char[][] grid2 = new char[][] {
+                {'c', 'b', ' '},
+                {'b', 'b', 'b'},
+                {' ', 'b', 'a'},
+                {' ', ' ', 'a'}
+            };
+        
+        CharGrid cg = new CharGrid(grid);
+        CharGrid cg2 = new CharGrid(grid2);
+        
+        assertEquals(1, cg.countPlus());
+        assertEquals(1, cg2.countPlus());
+
+    }
+    @Test
+    //large grid
+    public void testCountPlus3() {
+        char[][] grid = new char[][] {
+                {' ', ' ', 'p', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', 'p', ' ', ' ', ' ', ' ', 'x', ' '},
+                {'p', 'p', 'p', 'p', 'p', ' ', 'x', 'x', 'x'},
+                {' ', ' ', 'p', ' ', ' ', 'y', ' ', 'x', ' '},
+                {' ', ' ', 'p', ' ', 'y', 'y', 'y', ' ', ' '},
+                {'z', 'z', 'z', 'z', 'z', 'y', 'z', 'z', 'z'},
+                {' ', ' ', 'x', 'x', ' ', 'y', ' ', ' ', ' '},
+            };
+
+        
+        CharGrid cg = new CharGrid(grid);
+        
+        assertEquals(2, cg.countPlus());
+
+    }
 }
